7ae2ad3bbffa47e8463d3a8a8c1c334ee14036bb
Brady Parkinson <brady.park12@gmail.com>
2023-07-29 14:22:47 +1200

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -20,6 +20,9 @@ public class Greeter {
     String language = args[0];
     String country = args[1];
     String name = args[2];
+    if (args.length <= 3) {
+      System.out.println("The length of args is " + args.length)
+    }
     Greeter greeter = new Greeter(language, country, name);
     System.out.println(greeter.sayHello());
   }
